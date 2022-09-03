57666e67161e54daa6845a370cb5d523f05b5e55
gitlinux12 <mshirole94@gmail.com>
2022-09-03 23:34:21 +0530

    Add Calculate Length of line

--- LineComparisionProblem.java
+++ LineComparisionProblem.java
@@ -8,8 +8,6 @@ public class LineComparisionProblem {
 	
 	public static void main(String[] args) {
 		
-		
-		
 		System.out.println("Welcome To Line Comparision Problem");
 		
 		LineComparisionProblem length= new LineComparisionProblem();
